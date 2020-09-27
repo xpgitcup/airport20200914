66d2b09a3e959b50ef23ced66e99325ea1366ded
cupcup <xpli126@gmail.com>
2020-09-27 22:31:15 +0800

    增加操作系统的判断

--- grails-app/init/airport20200914/BootStrap.groovy
+++ grails-app/init/airport20200914/BootStrap.groovy
@@ -1,15 +1,21 @@
 package airport20200914
 
+import cn.edu.cup.system.OsInfo
+
 class BootStrap {
 
     def initService
 
     def init = { servletContext ->
+        println("当前操作系统：${OsInfo.getOSname()}")
         def webRootDir = servletContext.getRealPath("/")
         println("当前系统根路径：${webRootDir}")
-
-        initService.setSampleData()
-        initService.loadScripts("${webRootDir}sql")
+        environments {
+            development {
+                initService.setSampleData()
+                initService.loadScripts("${webRootDir}sql")
+            }
+        }
     }
 
     def destroy = {
